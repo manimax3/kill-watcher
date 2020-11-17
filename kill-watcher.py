a1a2efaf73d1b84531b41b8046c481f56b2e88ff
Maximilian Schiller <manimax3@outlook.de>
2020-11-17 17:13:46 +0100

    set rallypoint via reactions

--- kill-watcher.py
+++ kill-watcher.py
@@ -6,14 +6,33 @@ import discord
 import toml
 import mysql.connector as connector
 import logging as log
+import re
+import redis
+from datetime import datetime
 
 class SystemsManager:
     def __init__(self, mapid):
         self.systems = []
         self.mapid = mapid
+        self.kills = []
+
+    def fetch_systems(self, mapid):
+        con = connector.connect(user=db["user"], password=db["password"],
+                                host=db["host"], port=db["port"], database=db["pathfinder_name"])
+        cur = con.cursor()
+
+        cur.execute(f"SELECT system.systemId, system.id FROM {db['pathfinder_name']}.map, {db['pathfinder_name']}.system "
+                    "WHERE system.active <> 0 AND map.id = system.mapId AND map.id = %s", (mapid,))
+
+        results = [row for row in cur]
+
+        cur.close()
+        con.close()
+
+        return results
 
     def update(self):
-        new_systems = fetch_systems(self.mapid)
+        new_systems = self.fetch_systems(self.mapid)
         gone_systems = set(self.systems) - set(new_systems)
         added_systems = set(new_systems) - set(self.systems)
         self.systems = new_systems
@@ -26,29 +45,45 @@ class SystemsManager:
 
         return commands
 
+    def remember_kill(self, killid, systemid):
+        self.kills.append((killid, systemid))
+
+    def set_rally_point(self, system_id):
+        con = connector.connect(user=db["user"], password=db["password"],
+                                host=db["host"], port=db["port"], database=db["pathfinder_name"])
+        cur = con.cursor()
+
+        timestamp = datetime.utcnow().strftime("%Y-%m-%d %T")
+
+        cur.execute(f"UPDATE {db['pathfinder_name']}.system SET updated = %s, rallyUpdated = system.updated "
+                    "WHERE system.systemId = %s", (timestamp, system_id))
+
+        cur.close()
+        con.commit()
+        con.close()
+
+        r = redis.Redis(host=red_conf["host"], port=red_conf["port"])
+        r.flushall(True)
+
+    def get_system_of_kill(self, kill_id):
+        for k, s in self.kills:
+            if k == kill_id:
+                return s
+        return None
+
 
 config = toml.load("config.toml")
 db = config["db"]
+red_conf = config["redis"]
 
 esi_endpoint = "https://esi.evetech.net/latest"
 log.basicConfig(format='%(asctime)s %(message)s', level=getattr(log, config["watcher"]["loglevel"], log.INFO))
 
 discord_client = discord.Client()
+sm = SystemsManager(config["watcher"]["mapid"])
 
-def fetch_systems(mapid):
-    con = connector.connect(user=db["user"], password=db["password"],
-                            host=db["host"], port=db["port"], database=db["pathfinder_name"])
-    cur = con.cursor()
-
-    cur.execute(f"SELECT system.systemId FROM {db['pathfinder_name']}.map, {db['pathfinder_name']}.system "
-                "WHERE system.active <> 0 AND map.id = system.mapId AND map.id = %s", (mapid,))
-
-    results = [row for row in cur]
-
-    cur.close()
-    con.close()
-
-    return results
+re_killurl = re.compile(r"https://zkillboard\.com/kill/([0-9]+)/")
+re_sysname = re.compile(r"Kill occurred in (.*)\n")
 
 async def consumer(msg):
     msg = json.loads(msg)
@@ -103,6 +138,7 @@ async def consumer(msg):
     else:
         ping_role = ""
 
+    sm.remember_kill(msg["killID"], killmail["solar_system_id"])
     await channel.send(f"{ping_role}Kill occurred in {system['name']}\n{msg['url']}")
 
 async def consumer_handler(websocket):
@@ -112,7 +148,6 @@ async def consumer_handler(websocket):
         await asyncio.sleep(0.5)
 
 async def producer_handler(websocket):
-    sm = SystemsManager(config["watcher"]["mapid"])
     await websocket.send(json.dumps({"action": "sub", "channel": "public"}))
     while True:
         commands = sm.update()
@@ -130,5 +165,52 @@ async def connect():
 @discord_client.event
 async def on_ready():
     log.info(f"Initialized {discord_client.user.name}")
+
+@discord_client.event
+async def on_raw_reaction_add(payload):
+    channel = discord_client.get_channel(config["discord"]["channel_id"])
+
+    if payload.channel_id != channel.id:
+        return
+
+    msg = await channel.fetch_message(payload.message_id)
+
+    if msg.author != discord_client.user:
+        return
+
+    match = re_killurl.search(msg.content)
+
+    if match is None:
+        log.info("Reaction did not contain a zkillboard link. Skipping")
+        return
+
+    killid = int(match.groups(1)[0])
+    system_id = sm.get_system_of_kill(killid)
+
+    if system_id is None:
+        log.info(f"Could not lookup systemid for kill {killid}. Asking zkillboard")
+        async with aiohttp.ClientSession() as s:
+            async with s.get(
+                    f"https://zkillboard.com/api/killID/{killid}/") \
+                    as response:
+                zkb_data = await response.json()
+                zkb_data = zkb_data[0]["zkb"]
+                killhash = zkb_data["hash"]
+            async with s.get(
+                f"{esi_endpoint}/killmails/{killid}/{killhash}/?datasource=tranquility") \
+                    as response:
+                killmail = await response.json()
+                system_id = killmail["solar_system_id"]
+        if system_id is not None and killid is not None:
+            sm.remember_kill(killid, system_id)
+
+    sm.set_rally_point(system_id)
+
+    match = re_sysname.search(msg.content)
+
+    if match is not None:
+        sysname = match.groups(1)[0]
+        await channel.send(f"Rally point set to {sysname}.")
+
 
 asyncio.get_event_loop().run_until_complete(connect())
